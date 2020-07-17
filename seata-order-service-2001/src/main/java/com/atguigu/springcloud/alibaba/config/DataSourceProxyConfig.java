0bba1749c83939eb1688091f3064ca9f82dff647
ZhengHaoYun <1013431730@qq.com>
2020-07-17 18:01:58 +0800

    seata config

--- seata-order-service-2001/src/main/java/com/atguigu/springcloud/alibaba/config/DataSourceProxyConfig.java
+++ seata-order-service-2001/src/main/java/com/atguigu/springcloud/alibaba/config/DataSourceProxyConfig.java
@@ -10,11 +10,10 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
 import javax.sql.DataSource;
 
 /**
- * @auther zzyy
- * @create 2019-12-11 16:58
  * 使用Seata对数据源进行代理
  */
 @Configuration
@@ -25,7 +24,7 @@ public class DataSourceProxyConfig {
 
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource druidDataSource(){
+    public DataSource druidDataSource() {
         return new DruidDataSource();
     }
 
